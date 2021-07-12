9627e8d10abdaad61cc2f0fde9c6f90fab08a55d
Sylvain Laconi <sylvain.laconi@gmail.com>
2021-07-12 10:02:23 +0200

    fist commit

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Accelerometer } from "expo-sensors";
 
 export default function App() {
+  const [data, setData] = useState({
+    x: -1,
+    y: 0,
+    z: 1,
+  });
+
+  const [subscription, setSubscription] = useState(null);
+
+  const _subscribe = () => {
+    setSubscription(
+      Accelerometer.addListener((accelerometerData) => {
+        setData(accelerometerData);
+      })
+    );
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
+  const { x, y, z } = data;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>
+        Accelerometer: (in Gs where 1 G = 9.81 m s^-2)
+      </Text>
+      <Text style={styles.text}>
+        x: {Math.round(x)} y: {Math.round(y)} z: {Math.round(z)}
+      </Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 12,
+    textAlign: "center",
   },
 });
